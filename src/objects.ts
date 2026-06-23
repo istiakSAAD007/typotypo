c9da838df4c25318bbf392987af193431b7a2a8e
istiakSAAD007 <s12a12saadabir@gmail.com>
2026-06-23 16:23:19 +0600

    object type

---
+++ src/objects.ts
@@ -0,0 +1,13 @@
+type Car = {
+  name: string;
+  brand: string;
+  model: string;
+};
+
+const anyCar: Car = {
+  name: "BunnyFooty",
+  brand: "BMW",
+  model: "M440I",
+};
+
+console.log(anyCar);
